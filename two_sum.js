18653fb0688bae00f649be9ff3f2d5754152c343
Majda-Fg <majda.fougou@gmail.com>
2025-05-19 14:10:39 +0200

    exercice two sum done

--- two_sum.js
+++ two_sum.js
@@ -1,4 +1,14 @@
 function twoSum(numbers, target){
 
+let result = []
+
+for(let i = 0; i< numbers.length; i++){
+    for(let j = 0; j< numbers.length; j++){
+        if(numbers[i] + numbers[j] === target){
+            result.push(numbers.indexOf(numbers[i]))
+        }
+    }
+    }
+    return result
 }
-console.log(twoSum())
+console.log(twoSum([2, 7, 11, 15], 9))
